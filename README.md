e0e3314b6693130408fc3350bc32cdc59ad712d0
kor <kor.dwarshuis@gmail.com>
2013-10-14 21:07:13 +0200

    added sound, JS in external file, added config object

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 oldMovieEffectSlideDeck
 =======================
 
-Simpel slide deck with an old movie effect.
+Simpel slide deck with an old movie effect. Created with ”HTML5” (CSS3).
 
 
 <a href="http://www.woudziel.nl/githubdemo/oldMovieEffectSlideDeck/">demo</a>
+
+I use a shortened version of <a href="http://freesound.org/people/Incarnadine/sounds/16997/">this sound recorded by Incarnadine</a>
---
+++ js/oldMovieEffectSlideDeck.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2013 Kor Dwarshuis <kor@dwarshuis.com>
+ * Released under the MIT license.
+ */
+
+(function createAudioElement(args) { // background sound
+    "use strict";
+    var fragment = document.createDocumentFragment();
+    var audio = fragment.appendChild(document.createElement('audio'));
+    audio.setAttribute("loop", true);
+    var source = (document.createElement('source'));
+    source.setAttribute("src", args.path + ".mp3");
+    source.setAttribute("type", "audio/mpeg");
+    var source2 = (document.createElement('source'));
+    source2.setAttribute("src", args.path + ".ogg");
+    source2.setAttribute("type", "audio/ogg");
+    audio.appendChild(source);
+    audio.appendChild(source2);
+    document.getElementsByTagName('body').item(0).appendChild(fragment);
+    audio.volume = 0.5;
+    audio.play();
+}({
+    path: "sound/16997__incarnadine__modulated-radio-static-modified"
+}));
+
+
+
+(function(args) {
+    "use strict";
+    var allSlides = document.querySelectorAll(args.element);
+    var i = allSlides.length-1;
+    var keepLastSlideVisisble;
+    args.keepLastSlideVisisble ? keepLastSlideVisisble = 0 : keepLastSlideVisisble = -1;
+
+    setTimeout(k, args.pause);
+    function k(){
+        if (i > keepLastSlideVisisble) {
+            allSlides[i].style.display = "none";
+            i--;
+            setTimeout(k, args.pause);
+        }
+    }
+})(config);
