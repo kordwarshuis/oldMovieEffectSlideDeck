e37d0d57373d15cb887b8a7b92552dfec047e6ec
kor <kor.dwarshuis@gmail.com>
2013-10-16 23:38:04 +0200

    Slide order is reversed, so since it was reversed, it's now normal. Configuration for starting and ending animation added. Sound stops at the ending.

--- js/oldMovieEffectSlideDeck.js
+++ js/oldMovieEffectSlideDeck.js
@@ -3,43 +3,69 @@
  * Released under the MIT license.
  */
 
-(function createAudioElement(args) { // background sound
+
+var noise = (function () { // background sound
     "use strict";
     var fragment = document.createDocumentFragment();
     var audio = fragment.appendChild(document.createElement('audio'));
     audio.setAttribute("loop", true);
     var source = (document.createElement('source'));
-    source.setAttribute("src", args.path + ".mp3");
+    source.setAttribute("src", "sound/16997__incarnadine__modulated-radio-static-modified" + ".mp3");
     source.setAttribute("type", "audio/mpeg");
     var source2 = (document.createElement('source'));
-    source2.setAttribute("src", args.path + ".ogg");
+//    source2.setAttribute("src", args.path + ".ogg");
+    source2.setAttribute("src", "sound/16997__incarnadine__modulated-radio-static-modified" + ".ogg");
     source2.setAttribute("type", "audio/ogg");
     audio.appendChild(source);
     audio.appendChild(source2);
     document.getElementsByTagName('body').item(0).appendChild(fragment);
     audio.volume = 0.5;
-    audio.play();
-}({
-    path: "sound/16997__incarnadine__modulated-radio-static-modified"
-}));
+
+    var play = function () {
+        audio.play();
+    };
+    var stop = function () {
+        audio.pause(); // there is no stop method, this is an alternative
+        if (audio.currentTime !== 0) {
+            audio.currentTime = 0;
+        } // there is no stop method, this is an alternative
+    };
+
+    return {
+        play: play,
+        stop: stop
+    };
+
+}());
 
 
-
-(function(args) {
+var slideChanger = function (args) {
     "use strict";
-    var allSlides = document.querySelectorAll(args.element);
-    var i = allSlides.length-1;
-    var keepLastSlideVisisble;
-    args.keepLastSlideVisisble ? keepLastSlideVisisble = 0 : keepLastSlideVisisble = -1;
+    var allSlides = document.querySelectorAll("div");
 
-    setTimeout(k, args.pause);
-    function k(){
-        if (i > keepLastSlideVisisble) {
-            allSlides[i].style.display = "none";
-            i--;
-            setTimeout(k, args.pause);
+    var i = 0;
+
+    for (var j = 0; j < allSlides.length; j++) {
+        allSlides[j].style.display = 'none';
+    }
+
+    function stopAnimation() {
+        document.body.className = "";
+        noise.stop();
+    }
+
+    setTimeout(k, args.timeToWaitBeforeStartingAnimation);
+    function k() {
+        if (i < allSlides.length) {
+            allSlides[i].style.display = "block";
+            i++;
+            setTimeout(k, args.timeToShowSlide);
         } else {
-            document.body.className = ""; // stop animation
+
+            setTimeout(stopAnimation, args.timeToWaitBeforeStoppingAnimation); // stop animation
         }
     }
-})(config);
+};
+
+noise.play();
+slideChanger(config);
